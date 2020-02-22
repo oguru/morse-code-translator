7d61ea51c5565ebc792242740f917d452871597a
peter oboyle <peter.oboyle@Nology.local>
2020-02-22 00:07:13 +0000

    new changes

--- main.js
+++ main.js
@@ -1,71 +1,68 @@
-const morse =
-    {
-        ".-": "A",
-        "-...": "B",
-        "-.-.": "C",
-        "-..": "D",
-        ".": "E",
-        "..-.": "F",
-        "--.": "G",
-        "....": "H",
-        "..": "I",
-        ".---": "J",
-        "-.-": "K",
-        ".-..": "L",
-        "--": "M",
-        "-.": "N",
-        "---": "O",
-        ".--.": "P",
-        "--.-": "Q",
-        ".-.": "R",
-        "...": "S",
-        "-": "T",
-        "..-": "U",
-        "...-": "V",
-        ".--": "W",
-        "-..-": "X",
-        "-.--": "Y",
-        "--..": "Z",
-        "-----": "0",
-        ".----": "1",
-        "..---": "2",
-        "...--": "3",
-        "....-": "4",
-        ".....": "5",
-        "-....": "6",
-        "--...": "7",
-        "---..": "8",
-        "----.": "9"
-    };
+const morseCode = {
+    a: '._',
+    b: '_...',
+    c: '_._.',
+    d: '_..',
+    e: '.',
+    f: '.._.',
+    g: '__.',
+    h: '....',
+    i: '..',
+    j: '.___',
+    k: '_._',
+    l: '._..',
+    m: '__',
+    n: '_.',
+    o: '___',
+    p: '.__.',
+    q: '__._',
+    r: '._.',
+    s: '...',
+    t: '_',
+    u: '.._',
+    v: '..._',
+    w: '.__',
+    x: '_.._',
+    y: '_.__',
+    z: '__..',
+    0: '_____',
+    1: '.____',
+    2: '..___',
+    3: '...__',
+    4: '...._',
+    5: '.....',
+    6: '_....',
+    7: '__...',
+    8: '___..',
+    9: '____.'
+};
 
 
 //    const convert = morseObject[0][morseValue];
 
-//get input from html and put as input to function with array
+//get input from html and split into string array for each letter
 //
 
 const translate = (morseObject, toTranslate) => {
 
-    const stringArray = toTranslate.split("");
+    const stringArray = toTranslate.split(""); //split input into array of arrays
+    console.log(stringArray);
     
     // const convert = letter => {
     //     return Object.keys(morseObject).find(key => morseObject[key]===letter);
     // }
- 
-        const morseArray = Object.entries(morseObject);
 
-    const testArray = value => {
-        
-        const checkInside = value.filter(newValue => newValue.includes("P"));
-        console.log(checkInside);
-        
-        return checkInside;
-    };
+    const convert = keyAndValue => {
+        return keyAndValue.hasOwnProperty(keyAndValue);
+    }
+
+    const converted = morseObject.map(convert);
     
-
-    const getBoth = morseArray.filter(testArray);
+    console.log(converted);
+    
+    // const getBoth = morseArray.filter(testArray);
     
     
 }
 
-translate(morse, "PETER");
+translate(morseCode, "PETER");
